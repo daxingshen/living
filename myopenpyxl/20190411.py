2b78b802538c6b2b8ed13b831823b72b5029fc2c
DESKTOP-P3P1CGO\guanj <guanjun.xing6@gmail.com>
2019-04-11 23:53:20 +0800

    no message

--- myopenpyxl/20190411.py
+++ myopenpyxl/20190411.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+import openpyxl
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
+ws1 = wb.create_sheet('sheet1')
+ws2 = wb.create_sheet('sheet2')
+ws.title = "New Title"
+ws.sheet_properties.tabColor = "1072BA"
+print(wb.sheetnames)
+
+for sheet in wb:
+    print(sheet.title)
+
+target = wb.copy_worksheet(ws)
+wb.save('./data/1.xlsx')
+wb.close()
